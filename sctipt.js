6a88c4a5b05df6966aa19f646c3043b406dd38cc
ImanTehrani@gmail.com <ImanTehrani@gmail.com>
2023-05-22 16:14:18 +0430

    Sortable with Save Position

--- sctipt.js
+++ sctipt.js
@@ -1,51 +1,44 @@
 if(localStorage.getItem('Position')){
-  let array = JSON.parse(localStorage.getItem("Position"))
-  
-  $.each(array , (i,item)=>{
-      let Containers = $(`#${item.ContainerId}`)
-      let Child = item.Childs
-     
-      //if(Child == undefined){
-      //    Containers.append('')
-      //}
-      //for(let i=0 ; i < Containers.length ;  i++){
-      //    Containers.append($(`#${Child}`)[i])
-      //}
-      //$.each(Child , (i , item)=>{
-      //    Containers.append($(`#${item}`)[i])
-      //})
-  })
+    let array = JSON.parse(localStorage.getItem("Position"))
+    $.each(array , (i,item)=>{
+        let Containers = $(`#${item.ContainerId}`)
+        let Child = item.Childs
+        $.each(Child , (i , item)=>{
+            let items = $(`#${item}`)
+            $(Containers).append(items)
+        })
+    })
 }
 
 $('.Box').sortable({
-  receive:function(ev,ui){
-      if($('#Box1').children().length > 2){
-          ui.sender.sortable('cancel')
-      }
-  },
-  update: function (event, ui) {
-      let Arr =[]
-      $('.Box').each(function(evt,item){
-          let Obj = {}
-          let Container = $(item).attr('id')
-          Obj.ContainerId = Container
-          let Childs = []
-          $(item).children().each(function(e,i){
-              let BoxesID = $(i).attr('id')
-              Childs.push(BoxesID)
-              Obj.Childs = Childs
-          })
-          Arr.push(Obj)
-      })
+    receive:function(ev,ui){
+        if($('#Box1').children().length > 2){
+            ui.sender.sortable('cancel')
+        }
+    },
+    update: function (event, ui) {
+        let Arr =[]
+        $('.Box').each(function(evt,item){
+            let Obj = {}
+            let Container = $(item).attr('id')
+            Obj.ContainerId = Container
+            let Childs = []
+            $(item).children().each(function(e,i){
+                let BoxesID = $(i).attr('id')
+                Childs.push(BoxesID)
+                Obj.Childs = Childs
+            })
+            Arr.push(Obj)
+        })
 
-      localStorage.setItem('Position' , JSON.stringify(Arr))
-  },
-  connectWith:'.Box',
-  handle: '.DragIcon',
-  placeholder:'placeholder',
-  start: function(event, ui) {
-      ui.placeholder.html(ui.item.html());
-  } , 
-  
+        localStorage.setItem('Position' , JSON.stringify(Arr))
+    },
+    connectWith:'.Box',
+    handle: '.DragIcon',
+    placeholder:'placeholder',
+    start: function(event, ui) {
+        ui.placeholder.html(ui.item.html());
+    } , 
+    
 })
 $(".Dashboard").disableSelection();
